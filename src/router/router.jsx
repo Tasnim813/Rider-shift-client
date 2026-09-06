e027e4275ad85cd3fce8cdc1e264b6a512cdd791
Tasnim813 <tasnim3j@gmail.com>
2026-09-06 23:53:52 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../page/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index:true,
+            Component: Home,
+
+        },
+        {
+            path:''
+        }
+    ]
+  },
+]);
